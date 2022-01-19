4fefe0316d46292b390252c724552d0a857f247d
borey uk <me@borey.uk>
2022-01-19 23:08:14 +0100

    update keymap led indicators
    updated caps lock leds indicator
    light up relevant keys when mod1 key is pressed
    light up side leds

--- keyboards/gmmk/pro/ansi/keymaps/borzjouzinage/config.h
+++ keyboards/gmmk/pro/ansi/keymaps/borzjouzinage/config.h
@@ -1,5 +1,27 @@
 #pragma once
 
-// Define all the RGB effects
-#define RGB_MATRIX_KEYPRESSES
-#define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+// RGB Effects configuration
+#ifdef RGB_MATRIX_ENABLE
+    #define RGB_MATRIX_KEYPRESSES // Reactive and splash
+    #define RGB_MATRIX_FRAMEBUFFER_EFFECTS // Heatmap, rain
+    #define RGB_DISABLE_WHEN_USB_SUSPENDED
+    
+    // Disabling some RGB effects
+    #undef ENABLE_RGB_MATRIX_GRADIANT_UP_DOWN    // top-down rainbow  
+    #undef ENABLE_RGB_MATRIX_GRADIENT_LEFT_RIGHT // left-right rainbow
+    #undef ENABLE_RGB_MATRIX_BAND_PINWHEEL_SAT // pinwheel effects
+    #undef ENABLE_RGB_MATRIX_BAND_PINWHEEL_VAL
+    #undef ENABLE_RGB_MATRIX_BAND_SPIRAL_SAT
+    #undef ENABLE_RGB_MATRIX_BAND_SPIRAL_VAL
+    #undef ENABLE_RGB_MATRIX_CYCLE_ALL         // rainbow effects
+    #undef ENABLE_RGB_MATRIX_CYCLE_UP_DOWN
+    #undef ENABLE_RGB_MATRIX_CYCLE_OUT_IN
+    #undef ENABLE_RGB_MATRIX_CYCLE_OUT_IN_DUAL
+    #undef ENABLE_RGB_MATRIX_RAINBOW_MOVING_CHEVRON
+    #undef ENABLE_RGB_MATRIX_CYCLE_SPIRAL
+    #undef ENABLE_RGB_MATRIX_DUAL_BEACON              
+    #undef ENABLE_RGB_MATRIX_RAINBOW_BEACON             
+    #undef ENABLE_RGB_MATRIX_RAINBOW_PINWHEELS 
+    #undef ENABLE_RGB_MATRIX_RAINDROPS
+    #undef ENABLE_RGB_MATRIX_JELLYBEAN_RAINDROPS
+#endif
--- keyboards/gmmk/pro/ansi/keymaps/borzjouzinage/keymap.c
+++ keyboards/gmmk/pro/ansi/keymaps/borzjouzinage/keymap.c
@@ -16,6 +16,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include QMK_KEYBOARD_H
 
+#include "rgb_matrix_map.h"
+
 #define _LAYER0 0
 #define _LAYER1 1
 
@@ -25,11 +27,28 @@ enum custom_keycodes {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-
-    [_LAYER0] = LAYOUT(KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_PSCR, KC_MPLY, KC_NUBS, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, KC_PGUP, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS, KC_PGDN, KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT, KC_DEL, KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP, KC_MNXT, KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_RALT, MO(1), KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT),
-
-    [_LAYER1] = LAYOUT(KC_TRNS, KC_MYCM, KC_WHOM, KC_CALC, KC_MSEL, KC_MPRV, KC_MNXT, KC_MPLY, KC_MSTP, KC_MUTE, KC_VOLD, KC_VOLU, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RGB_TOG, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MPRV, KC_TRNS, KC_TRNS, RGB_VAI, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RESET, KC_TRNS, KC_TRNS, KC_TRNS, RGB_VAD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_HOME, KC_TRNS, KC_TRNS, RGB_HUI, KC_TRNS, KC_TRNS, KC_TRNS, NK_TOGG, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RGB_MOD, KC_END, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RGB_SPD, RGB_RMOD, RGB_SPI)
-
+//      ESC      F1       F2       F3       F4       F5        F6       F7       F8       F9       F10      F11      F12      Prt           Rotary(Mute)
+//      ~        1        2        3        4        5         6        7        8        9        0         -       (=)      BackSpc           Del
+//      Tab      Q        W        E        R        T         Y        U        I        O        P        [        ]        \                 PgUp
+//      Caps     A        S        D        F        G         H        J        K        L        ;        "                 Enter             PgDn
+//      Sh_L              Z        X        C        V         B        N        M        ,        .        ?                 Sh_R     Up       End
+//      Ct_L     Win_L    Alt_L                                SPACE                               Alt_R    FN       Ct_R     Left     Down     Right
+    [_LAYER0] = LAYOUT(
+        KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_PSCR, KC_MPLY,
+        KC_NUBS, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, KC_PGUP,
+        KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS, KC_PGDN,
+        KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT, KC_DEL,
+        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP, KC_MNXT,
+        KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_RALT, MO(1), KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT),
+    
+    [_LAYER1] = LAYOUT(
+        KC_TRNS, KC_MYCM, KC_WHOM, KC_CALC, KC_MSEL, KC_MPRV, KC_MNXT, KC_MPLY, KC_MSTP, KC_MUTE, KC_VOLD, KC_VOLU, KC_TRNS, KC_PWR, KC_TRNS,
+        KC_TRNS, BL_TOGG, RGB_TOG, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_HOME,
+        KC_TRNS, BL_INC, RGB_VAI, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RESET, KC_END,
+        KC_TRNS, BL_DEC, RGB_VAD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_INS,
+        KC_TRNS, BL_STEP, RGB_HUI, KC_TRNS, KC_TRNS, KC_TRNS, NK_TOGG, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RGB_MOD, KC_MPRV,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RGB_SPD, RGB_RMOD, RGB_SPI) 
+    
 };
 
 #ifdef ENCODER_ENABLE
@@ -43,176 +62,124 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
 }
 #endif  // ENCODER_ENABLE
 
-// Caps lock indicator, adapted from andrebrait
 #ifdef RGB_MATRIX_ENABLE
+static void set_rgb_caps_leds_on(void);
+static void set_rgb_caps_leds_off(void);
+static void set_france_side_leds_on(void);
+// static void set_side_leds_off(void);
 
-/* Renaming those to make the purpose on this keymap clearer */
-#    define LED_FLAG_CAPS LED_FLAG_NONE
-#    define LED_FLAG_EFFECTS LED_FLAG_INDICATOR
-
-static void set_rgb_caps_leds(void);
-
-#    if RGB_CONFIRMATION_BLINKING_TIME > 0
-static uint16_t effect_started_time = 0;
-static uint8_t  r_effect = 0x0, g_effect = 0x0, b_effect = 0x0;
-static void     start_effects(void);
-
-/* The higher this is, the slower the blinking will be */
-#        ifndef TIME_SELECTED_BIT
-#            define TIME_SELECTED_BIT 8
-#        endif
-#        if TIME_SELECTED_BIT < 0 || TIME_SELECTED_BIT >= 16
-#            error "TIME_SELECTED_BIT must be a positive integer smaller than 16"
-#        endif
-#        define effect_red() r_effect = 0xFF, g_effect = 0x0, b_effect = 0x0
-#        define effect_green() r_effect = 0x0, g_effect = 0xFF, b_effect = 0x0
-#    endif  // RGB_CONFIRMATION_BLINKING_TIME > 0
-
-bool led_update_user(led_t led_state) {
-    if (led_state.caps_lock) {
-        if (!rgb_matrix_is_enabled()) {
-            /* Turn ON the RGB Matrix for CAPS LOCK */
-            rgb_matrix_set_flags(LED_FLAG_CAPS);
-            rgb_matrix_enable();
-        }
-    } else if (rgb_matrix_get_flags() == LED_FLAG_CAPS) {
-        /* RGB Matrix was only ON because of CAPS LOCK. Turn it OFF. */
-        rgb_matrix_set_flags(LED_FLAG_ALL);
-        rgb_matrix_disable();
-    }
-    return true;
-}
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case RGB_MOD:
-        case RGB_RMOD:
-        case RGB_HUI:
-        case RGB_HUD:
-        case RGB_SAI:
-        case RGB_SAD:
-        case RGB_VAI:
-        case RGB_VAD:
-        case RGB_SPI:
-        case RGB_SPD:
-            if (record->event.pressed) {
-                if (rgb_matrix_get_flags() != LED_FLAG_ALL) {
-                    /* Ignore changes to RGB settings while only it's supposed to be OFF */
-                    return false;
-                }
-            }
-            break;
-        case RGB_TOG:
-            if (record->event.pressed) {
-                if (rgb_matrix_is_enabled()) {
-                    switch (rgb_matrix_get_flags()) {
-#    if RGB_CONFIRMATION_BLINKING_TIME > 0
-                        case LED_FLAG_EFFECTS:
-#    endif
-                        case LED_FLAG_CAPS:
-                            /* Turned ON because of EFFECTS or CAPS, is actually OFF */
-                            /* Change to LED_FLAG_ALL to signal it's really ON */
-                            rgb_matrix_set_flags(LED_FLAG_ALL);
-                            /* Will be re-enabled by the processing of the toggle */
-                            rgb_matrix_disable_noeeprom();
-                            break;
-                        case LED_FLAG_ALL:
-/* Is actually ON */
-#    if RGB_CONFIRMATION_BLINKING_TIME > 0
-                            if (effect_started_time > 0) {
-                                /* Signal EFFECTS */
-                                rgb_matrix_set_flags(LED_FLAG_EFFECTS);
-                                /* Will be re-enabled by the processing of the toggle */
-                                rgb_matrix_disable_noeeprom();
-                            } else
-#    endif
-                                if (host_keyboard_led_state().caps_lock) {
-                                /* Signal CAPS */
-                                rgb_matrix_set_flags(LED_FLAG_CAPS);
-                                /* Will be re-enabled by the processing of the toggle */
-                                rgb_matrix_disable_noeeprom();
-                            }
-                            break;
-                    }
-                }
-            }
-            break;
-    }
-    return true;
-}
-
-void rgb_matrix_indicators_user() {
-#    if RGB_CONFIRMATION_BLINKING_TIME > 0
-    if (effect_started_time > 0) {
-        /* Render blinking EFFECTS */
-        uint16_t deltaTime = sync_timer_elapsed(effect_started_time);
-        if (deltaTime <= RGB_CONFIRMATION_BLINKING_TIME) {
-            uint8_t led_state = ((~deltaTime) >> TIME_SELECTED_BIT) & 0x01;
-            uint8_t val_r     = led_state * r_effect;
-            uint8_t val_g     = led_state * g_effect;
-            uint8_t val_b     = led_state * b_effect;
-            rgb_matrix_set_color_all(val_r, val_g, val_b);
-            if (host_keyboard_led_state().caps_lock) {
-                set_rgb_caps_leds();
-            }
-            return;
-        } else {
-            /* EFFECTS duration is finished */
-            effect_started_time = 0;
-            if (rgb_matrix_get_flags() == LED_FLAG_EFFECTS) {
-                /* It was turned ON because of EFFECTS */
-                if (host_keyboard_led_state().caps_lock) {
-                    /* CAPS is still ON. Demote to CAPS */
-                    rgb_matrix_set_flags(LED_FLAG_CAPS);
-                } else {
-                    /* There is nothing else keeping RGB enabled. Reset flags and turn if off. */
-                    rgb_matrix_set_flags(LED_FLAG_ALL);
-                    rgb_matrix_disable_noeeprom();
-                }
-            }
+void rgb_matrix_indicators_user()
+{
+    if (host_keyboard_led_state().caps_lock){
+        set_rgb_caps_leds_on();
+    } else {
+        if(rgb_matrix_get_flags() == LED_FLAG_NONE){
+            set_rgb_caps_leds_off();
         }
     }
-#    endif  // RGB_CONFIRMATION_BLINKING_TIME > 0
-    if (rgb_matrix_get_flags() == LED_FLAG_CAPS) {
-        rgb_matrix_set_color_all(0x0, 0x0, 0x0);
-    }
-    if (host_keyboard_led_state().caps_lock) {
-        set_rgb_caps_leds();
+
+    switch(get_highest_layer(layer_state)){
+        case _LAYER0:
+            set_france_side_leds_on();
+            break;
+        case _LAYER1:
+            set_france_side_leds_on();
+            rgb_matrix_set_color(LED_PRT, 255, 0, 0);
+            rgb_matrix_set_color(LED_PGUP, 255, 255, 255);
+            rgb_matrix_set_color(LED_PGDN, 255, 255, 255);
+            rgb_matrix_set_color(LED_MPLY, 255, 255, 255);
+            rgb_matrix_set_color(LED_DEL, 255, 255, 255);
+
+            rgb_matrix_set_color(LED_2, 255, 255, 255);
+            rgb_matrix_set_color(LED_W, 0, 255, 0);
+            rgb_matrix_set_color(LED_S, 255, 0, 0);
+            rgb_matrix_set_color(LED_X, 0, 0, 255);
+
+            rgb_matrix_set_color(LED_N, 255, 255, 255);
+
+            rgb_matrix_set_color(LED_LEFT, 255, 0, 0);
+            rgb_matrix_set_color(LED_RIGHT, 0, 255, 0);
+            rgb_matrix_set_color(LED_UP, 0, 0, 255);
+            rgb_matrix_set_color(LED_DOWN, 0, 0, 255);
+            break;
     }
 }
 
-#    if RGB_CONFIRMATION_BLINKING_TIME > 0
-static void start_effects() {
-    effect_started_time = sync_timer_read();
-    if (!rgb_matrix_is_enabled()) {
-        /* Turn it ON, signal the cause (EFFECTS) */
-        rgb_matrix_set_flags(LED_FLAG_EFFECTS);
-        rgb_matrix_enable_noeeprom();
-    } else if (rgb_matrix_get_flags() == LED_FLAG_CAPS) {
-        /* It's already ON, promote the cause from CAPS to EFFECTS */
-        rgb_matrix_set_flags(LED_FLAG_EFFECTS);
+static void set_rgb_caps_leds_on()
+{
+    rgb_matrix_set_color(LED_CAPS, 255, 0, 0);
+    rgb_matrix_set_color(LED_TAB, 255, 0, 0);
+    rgb_matrix_set_color(LED_GRV, 255, 0, 0);
+    rgb_matrix_set_color(LED_LSFT, 255, 0, 0);
+    rgb_matrix_set_color(LED_LCTL, 255, 0, 0);
+
+    /*
+    for(int i = 0; i<NB_LED_SIDE; i++){
+        rgb_matrix_set_color(LED_SIDE_LEFT[i], 255, 0, 0);
+        rgb_matrix_set_color(LED_SIDE_RIGHT[i], 255, 0, 0);
     }
-}
-#    endif  // RGB_CONFIRMATION_BLINKING_TIME > 0
-
-static void set_rgb_caps_leds() {
-    rgb_matrix_set_color(67, 0xFF, 0x0, 0x0);  // Left side LED 1
-    rgb_matrix_set_color(68, 0xFF, 0x0, 0x0);  // Right side LED 1
-    rgb_matrix_set_color(70, 0xFF, 0x0, 0x0);  // Left side LED 2
-    rgb_matrix_set_color(71, 0xFF, 0x0, 0x0);  // Right side LED 2
-    rgb_matrix_set_color(73, 0xFF, 0x0, 0x0);  // Left side LED 3
-    rgb_matrix_set_color(74, 0xFF, 0x0, 0x0);  // Right side LED 3
-    rgb_matrix_set_color(76, 0xFF, 0x0, 0x0);  // Left side LED 4
-    rgb_matrix_set_color(77, 0xFF, 0x0, 0x0);  // Right side LED 4
-    rgb_matrix_set_color(80, 0xFF, 0x0, 0x0);  // Left side LED 5
-    rgb_matrix_set_color(81, 0xFF, 0x0, 0x0);  // Right side LED 5
-    rgb_matrix_set_color(83, 0xFF, 0x0, 0x0);  // Left side LED 6
-    rgb_matrix_set_color(84, 0xFF, 0x0, 0x0);  // Right side LED 6
-    rgb_matrix_set_color(87, 0xFF, 0x0, 0x0);  // Left side LED 7
-    rgb_matrix_set_color(88, 0xFF, 0x0, 0x0);  // Right side LED 7
-    rgb_matrix_set_color(91, 0xFF, 0x0, 0x0);  // Left side LED 8
-    rgb_matrix_set_color(92, 0xFF, 0x0, 0x0);  // Right side LED 8
-    rgb_matrix_set_color(3, 0xFF, 0x0, 0x0);   // CAPS LED
+    */
 }
 
-#endif  // RGB_MATRIX_ENABLE
+static void set_rgb_caps_leds_off()
+{
+    rgb_matrix_set_color(LED_CAPS, 0, 0, 0);
+    rgb_matrix_set_color(LED_TAB, 0, 0, 0);
+    rgb_matrix_set_color(LED_GRV, 0, 0, 0);
+    rgb_matrix_set_color(LED_LSFT, 0, 0, 0);
+    rgb_matrix_set_color(LED_LCTL, 0, 0, 0);
+    /*
+    for(int i = 0; i<NB_LED_SIDE; i++){
+        rgb_matrix_set_color(LED_SIDE_LEFT[i], 0, 0, 0);
+        rgb_matrix_set_color(LED_SIDE_RIGHT[i], 0, 0, 0);
+    }
+    */
+}
+
+static void set_france_side_leds_on()
+{
+    rgb_matrix_set_color(LED_SIDE_LEFT[0], 0, 0, 255);
+    rgb_matrix_set_color(LED_SIDE_LEFT[1], 0, 0, 255);
+    rgb_matrix_set_color(LED_SIDE_LEFT[2], 0, 0, 255);
+    rgb_matrix_set_color(LED_SIDE_LEFT[3], 255, 255, 255);
+    rgb_matrix_set_color(LED_SIDE_LEFT[4], 255, 255, 255);
+    rgb_matrix_set_color(LED_SIDE_LEFT[5], 255, 0, 0);
+    rgb_matrix_set_color(LED_SIDE_LEFT[6], 255, 0, 0);
+    rgb_matrix_set_color(LED_SIDE_LEFT[7], 255, 0, 0);
+
+    rgb_matrix_set_color(LED_SIDE_RIGHT[0], 255, 0, 0);
+    rgb_matrix_set_color(LED_SIDE_RIGHT[1], 255, 0, 0);
+    rgb_matrix_set_color(LED_SIDE_RIGHT[2], 255, 0, 0);
+
+    rgb_matrix_set_color(LED_SIDE_RIGHT[3], 255, 255, 255);
+    rgb_matrix_set_color(LED_SIDE_RIGHT[4], 255, 255, 255);
+
+    rgb_matrix_set_color(LED_SIDE_RIGHT[5], 0, 0, 255);
+    rgb_matrix_set_color(LED_SIDE_RIGHT[6], 0, 0, 255);
+    rgb_matrix_set_color(LED_SIDE_RIGHT[7], 0, 0, 255);
+}
+
+/*
+static void set_side_leds_off()
+{
+    rgb_matrix_set_color(LED_SIDE_LEFT[0], 0, 0, 0);
+    rgb_matrix_set_color(LED_SIDE_LEFT[1], 0, 0, 0);
+    rgb_matrix_set_color(LED_SIDE_LEFT[2], 0, 0, 0);
+    rgb_matrix_set_color(LED_SIDE_LEFT[3], 0, 0, 0);
+    rgb_matrix_set_color(LED_SIDE_LEFT[4], 0, 0, 0);
+    rgb_matrix_set_color(LED_SIDE_LEFT[5], 0, 0, 0);
+    rgb_matrix_set_color(LED_SIDE_LEFT[6], 0, 0, 0);
+    rgb_matrix_set_color(LED_SIDE_LEFT[7], 0, 0, 0);
+
+    rgb_matrix_set_color(LED_SIDE_RIGHT[0], 0, 0, 0);
+    rgb_matrix_set_color(LED_SIDE_RIGHT[1], 0, 0, 0);
+    rgb_matrix_set_color(LED_SIDE_RIGHT[2], 0, 0, 0);
+    rgb_matrix_set_color(LED_SIDE_RIGHT[3], 0, 0, 0);
+    rgb_matrix_set_color(LED_SIDE_RIGHT[4], 0, 0, 0);
+    rgb_matrix_set_color(LED_SIDE_RIGHT[5], 0, 0, 0);
+    rgb_matrix_set_color(LED_SIDE_RIGHT[6], 0, 0, 0);
+    rgb_matrix_set_color(LED_SIDE_RIGHT[7], 0, 0, 0);
+}
+*/
+
+#endif
